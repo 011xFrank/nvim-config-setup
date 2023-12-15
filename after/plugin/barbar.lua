720df9791af60a2faead73e1eec40393a0e54d55
011xFrank <francis.l.muhanji@gmail.com>
2023-12-15 12:52:51 +0300

    initial commit

--- after/plugin/barbar.lua
+++ after/plugin/barbar.lua
@@ -2,19 +2,14 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map('n', '<M-Right>', '<Cmd>BufferNext<CR>', opts)
-map('n', '<M-Left>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<space><Right>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<space><Left>', '<Cmd>BufferPrevious<CR>', opts)
 -- Goto buffer in position...
-map('n', '<M-1>', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<M-2>', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '<M-3>', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '<M-4>', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '<M-5>', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '<M-6>', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', '<M-7>', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<M-8>', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<M-9>', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', '<M-0>', '<Cmd>BufferLast<CR>', opts)
+map('n', '<space>1', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<space>2', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<space>3', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<space>4', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<space>0', '<Cmd>BufferLast<CR>', opts)
 
 map('n', '<M-c>', '<Cmd>BufferClose<CR>', opts)
 
