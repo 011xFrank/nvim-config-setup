1c060b0791a64fa1bfc8cef736e11a6e95eaf43f
011xFrank <francis.l.muhanji@gmail.com>
2023-12-16 10:10:54 +0300

    initial commit

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -3,6 +3,9 @@ local opt = { noremap = true, silent = true }
 
 require("toggleterm").setup({
   direction = "float",
+  float_opts = {
+    border = "curved"
+  }
 })
 
 keymap("n", "<leader>t", ":ToggleTerm<CR>",opt)
