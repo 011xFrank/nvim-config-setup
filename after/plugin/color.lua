a1f678458a3930a973190bdd43184784d8f6125e
011xFrank <francis.l.muhanji@gmail.com>
2023-12-16 10:17:45 +0300

    initial commit

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,8 +1,13 @@
-function colorScheme(color)
-  color = color or "everforest"
+function colorScheme(index)
+  color_schemes = {
+    "everforest",
+    "gruvbox"
+  }
+
+  color = color_schemes[index]
   vim.cmd.colorscheme(color)
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-colorScheme()
+colorScheme(1)
