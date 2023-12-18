050735be999882000dfcfa61e747c5abf39464f5
011xFrank <francis.l.muhanji@gmail.com>
2023-12-18 14:05:23 +0300

    initial commit

--- lua/011xfrank/options.lua
+++ lua/011xfrank/options.lua
@@ -1,5 +1,3 @@
-vim.cmd('set clipboard+=unnamedplus')
-
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
@@ -14,13 +12,14 @@ vim.opt.expandtab = true
 vim.opt.hidden = true
 vim.opt.title = true
 vim.opt.termguicolors = true
-vim.opt.filetype = "on"
 
+vim.opt.filetype = "on"
 vim.opt.mouse = 'a'
 vim.opt.fileencoding = 'utf-8'
 vim.opt.titlestring = 'NeoVim -- %t'
 vim.opt.signcolumn = "yes"
 vim.opt.foldmethod="indent"
+vim.opt.clipboard:append("unnamedplus")
 
 vim.opt.sidescrolloff = 10
 vim.opt.tabstop = 2
