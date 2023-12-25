83678399fed9707180691cc5825c4cb4e5a34255
011xFrank <francis.l.muhanji@gmail.com>
2023-12-25 19:49:45 +0300

    initial commit

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -4,7 +4,7 @@ require('rose-pine').setup({
 	--- @usage 'auto'|'main'|'moon'|'dawn'
 	variant = 'moon',
 	--- @usage 'main'|'moon'|'dawn'
-	dark_variant = 'moon',
+  dark_variant = 'main',
 	bold_vert_split = false,
 	dim_nc_background = false,
 	disable_background = false,
